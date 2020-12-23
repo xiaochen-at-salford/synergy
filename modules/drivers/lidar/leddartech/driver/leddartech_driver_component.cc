786976d7f625c46b896371c3570394a869cc72e3
mtetreault <alexandretm@amotus.ca>
2020-12-23 08:07:29 -0500

    leddartech: fill config info, device_name, serial_number, id_part_number, sw_part_number, mac_address

--- modules/drivers/lidar/leddartech/driver/leddartech_driver_component.cc
+++ modules/drivers/lidar/leddartech/driver/leddartech_driver_component.cc
@@ -33,16 +33,45 @@ LeddartechDriverComponent::~LeddartechDriverComponent() {
   }
 }
 bool LeddartechDriverComponent::Init() {
-  Config leddartech_config;
-  if (!GetProtoConfig(&leddartech_config)) {
+  Config ld_cfg;
+  if (!GetProtoConfig(&ld_cfg)) {
     return false;
   }
 
-  writer_ =
-      node_->CreateWriter<LeddartechScan>(leddartech_config.scan_channel());
-  drv_ = LeddartechDriverFactory::CreateDriver(leddartech_config);
+  writer_ = node_->CreateWriter<LeddartechScan>(ld_cfg.scan_channel());
+  drv_ = LeddartechDriverFactory::CreateDriver(ld_cfg);
 
   drv_->Init();
+
+  ld_cfg.set_device_name(
+      drv_->GetSensor()
+          ->GetProperties()
+          ->GetTextProperty(LeddarCore::LdPropertyIds::ID_DEVICE_NAME)
+          ->Value());
+
+  ld_cfg.set_serial_number(
+      drv_->GetSensor()
+          ->GetProperties()
+          ->GetTextProperty(LeddarCore::LdPropertyIds::ID_SERIAL_NUMBER)
+          ->Value());
+
+  ld_cfg.set_hw_part_number(
+      drv_->GetSensor()
+          ->GetProperties()
+          ->GetTextProperty(LeddarCore::LdPropertyIds::ID_PART_NUMBER)
+          ->Value());
+
+  ld_cfg.set_sw_part_number(
+      drv_->GetSensor()
+          ->GetProperties()
+          ->GetTextProperty(LeddarCore::LdPropertyIds::ID_SOFTWARE_PART_NUMBER)
+          ->Value());
+
+  ld_cfg.set_mac_address(
+      drv_->GetSensor()
+          ->GetProperties()
+          ->GetBufferProperty(LeddarCore::LdPropertyIds::ID_MAC_ADDRESS)
+          ->GetStringValue());
 
   running_ = true;
   device_thread_ = std::shared_ptr<std::thread>(new std::thread(
