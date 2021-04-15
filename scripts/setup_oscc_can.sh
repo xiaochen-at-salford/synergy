7c34fbb1d40ac813f8d3507bbe0ae862010f909a
Xiaochen Wang <x.wang20@salford.ac.uk>
2021-04-15 01:13:16 +0100

    Add OSCC can interface setup

--- scripts/setup_oscc_can.sh
+++ scripts/setup_oscc_can.sh
@@ -1,18 +1,6 @@
 TOP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 source ${TOP_DIR}/scripts/apollo.bashrc
 
-# can_dev="/dev/can0"
-# if [ ! -e "${can_dev}" ]; then
-#   warning "No CAN device named ${can_dev}. "
-#   exit
-# fi
-
-# can_dev="/dev/can1"
-# if [ ! -e "${can_dev}" ]; then
-#   warning "No CAN device named ${can_dev}. "
-#   exit
-# fi
-
 sudo ip link set can0 type can bitrate 500000
 sudo ip link set up can0
 
