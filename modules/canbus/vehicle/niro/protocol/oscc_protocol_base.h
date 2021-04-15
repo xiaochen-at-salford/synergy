b945e3cb66cd7cb20e9158e10820bd9728daafff
Xiaochen Wang <x.wang20@salford.ac.uk>
2021-04-15 01:39:37 +0100

    Add OSCC base protocl class

--- modules/canbus/vehicle/niro/protocol/oscc_protocol_base.h
+++ modules/canbus/vehicle/niro/protocol/oscc_protocol_base.h
@@ -7,7 +7,10 @@ namespace apollo {
 namespace canbus {
 namespace niro {
 
-class OsccProtocolBase {
+//TODO(xiaochen): Why decimal magical number is 1372 from the OSCC dbc file?
+
+class OsccProtocolBase 
+  : public ::apollo::drivers::canbus::ProtocolData<::apollo::canbus::ChassisDetail> {
  public:
   /**
    * @brief First magic byte used in commands and reports to distinguish CAN
@@ -15,23 +18,36 @@ class OsccProtocolBase {
    */
   static const int8_t OSCC_MAGIC_BYTE_0 = 0x05;
 
-/**
- * @brief Second magic byte used in commands and reports to distinguish CAN
- *        frame as coming from OSCC (and not OBD).
- */
+  /**
+   * @brief Second magic byte used in commands and reports to distinguish CAN
+   *        frame as coming from OSCC (and not OBD).
+   */
   static const int8_t OSCC_MAGIC_BYTE_1 = 0xCC;
+
+  bool is_active() const;
+
+  void activate();
+
+  void deactivate();
+
+  bool is_auto_active() const;
+  
+  void enable_auto_activation();
+
+  void disable_auto_activation();
+
+  bool use_magic() const;
+
+  void enable_magic_use();
+
+  void disable_magic_use();
 
   void set_p_magic(uint8_t *data);
 
  protected:
-  void set_use_magic_header(bool use_magic_header) 
-  { use_magic_header_ = use_magic_header; }
-
-  bool has_magic_header()
-  { return use_magic_header_; }
-
- private:
-  bool use_magic_header_ = false;
+  // bool is_active_ = false;
+  // bool is_auto_active_ = false;
+  bool use_magic_ = false;
 }; 
 
 }  // namespace niro
