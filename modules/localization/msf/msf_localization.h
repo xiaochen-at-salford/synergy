38bf2e3bdb7d9d532237602341d586da02231814
Xiaochen Wang <x.wang20@salford.ac.uk>
2021-04-21 09:42:09 +0100

    Refactor loclization module

--- modules/localization/msf/msf_localization.h
+++ modules/localization/msf/msf_localization.h
@@ -1,24 +1,3 @@
-/******************************************************************************
- * Copyright 2017 The Apollo Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *****************************************************************************/
-
-/**
- * @file msf_localization.h
- * @brief The class of MSFLocalization
- */
-
 #pragma once
 
 #include <memory>
@@ -41,20 +20,11 @@
 #include "modules/common/status/status.h"
 #include "modules/localization/msf/local_integ/localization_integ.h"
 
-/**
- * @namespace apollo::localization
- * @brief apollo::localization
- */
 namespace apollo {
 namespace localization {
 
 class LocalizationMsgPublisher;
 
-/**
- * @class MSFLocalization
- *
- * @brief generate localization info based on MSF
- */
 class MSFLocalization {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -63,31 +33,39 @@ class MSFLocalization {
   MSFLocalization();
 
   apollo::common::Status Init();
+
   void InitParams();
+
   void OnPointCloud(const std::shared_ptr<drivers::PointCloud> &message);
+
   void OnRawImu(const std::shared_ptr<drivers::gnss::Imu> &imu_msg);
+
   void OnRawImuCache(const std::shared_ptr<drivers::gnss::Imu> &imu_msg);
-  void OnGnssRtkObs(
-      const std::shared_ptr<drivers::gnss::EpochObservation> &raw_obs_msg);
-  void OnGnssRtkEph(
-      const std::shared_ptr<drivers::gnss::GnssEphemeris> &gnss_orbit_msg);
-  void OnGnssBestPose(
-      const std::shared_ptr<drivers::gnss::GnssBestPose> &bestgnsspos_msg);
-  void OnGnssHeading(
-      const std::shared_ptr<drivers::gnss::Heading> &gnss_heading_msg);
+
+  void OnGnssRtkObs(const std::shared_ptr<drivers::gnss::EpochObservation> &raw_obs_msg);
+
+  void OnGnssRtkEph(const std::shared_ptr<drivers::gnss::GnssEphemeris> &gnss_orbit_msg);
+
+  void OnGnssBestPose(const std::shared_ptr<drivers::gnss::GnssBestPose> &bestgnsspos_msg);
+
+  void OnGnssHeading(const std::shared_ptr<drivers::gnss::Heading> &gnss_heading_msg);
 
   void SetPublisher(const std::shared_ptr<LocalizationMsgPublisher> &publisher);
+
   void OnLocalizationTimer();
 
  private:
-  bool LoadGnssAntennaExtrinsic(const std::string &file_path, double *offset_x,
-                                double *offset_y, double *offset_z,
-                                double *uncertainty_x, double *uncertainty_y,
-                                double *uncertainty_z);
-  bool LoadImuVehicleExtrinsic(const std::string &file_path, double *quat_qx,
-                               double *quat_qy, double *quat_qz,
-                               double *quat_qw);
+  bool LoadGnssAntennaExtrinsic(
+      const std::string &file_path, 
+      double *offset_x, double *offset_y, double *offset_z, 
+      double *uncertainty_x, double *uncertainty_y, double *uncertainty_z );
+
+  bool LoadImuVehicleExtrinsic(
+      const std::string &file_path, 
+      double *quat_qx, double *quat_qy, double *quat_qz, double *quat_qw );
+
   bool LoadZoneIdFromFolder(const std::string &folder_path, int *zone_id);
+
   void CompensateImuVehicleExtrinsic(LocalizationEstimate *local_result);
 
  private:
--- modules/localization/msf/msf_localization_component.h
+++ modules/localization/msf/msf_localization_component.h
@@ -1,19 +1,3 @@
-/******************************************************************************
- * Copyright 2017 The Apollo Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *****************************************************************************/
-
 #pragma once
 
 #include <memory>
@@ -41,6 +25,7 @@ class MSFLocalizationComponent final
     : public cyber::Component<drivers::gnss::Imu> {
  public:
   MSFLocalizationComponent();
+
   ~MSFLocalizationComponent() = default;
 
   bool Init() override;
@@ -49,18 +34,17 @@ class MSFLocalizationComponent final
 
  private:
   bool InitConfig();
+
   bool InitIO();
 
  private:
   std::shared_ptr<cyber::Reader<drivers::PointCloud>> lidar_listener_ = nullptr;
   std::string lidar_topic_ = "";
 
-  std::shared_ptr<cyber::Reader<drivers::gnss::GnssBestPose>>
-      bestgnsspos_listener_ = nullptr;
+  std::shared_ptr<cyber::Reader<drivers::gnss::GnssBestPose>> bestgnsspos_listener_ = nullptr;
   std::string bestgnsspos_topic_ = "";
 
-  std::shared_ptr<cyber::Reader<drivers::gnss::Heading>>
-      gnss_heading_listener_ = nullptr;
+  std::shared_ptr<cyber::Reader<drivers::gnss::Heading>> gnss_heading_listener_ = nullptr;
   std::string gnss_heading_topic_ = "";
 
  private:
